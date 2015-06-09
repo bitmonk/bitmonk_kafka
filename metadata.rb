12f2bacc98bbe8049ddead3d470f8bd429963f51
Justin Alan Ryan <bitmonk@icloud.com>
2015-06-09 13:59:51 -0700

    initial cookbook generation.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'bitmonk_kafka'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures bitmonk_kafka'
+long_description 'Installs/Configures bitmonk_kafka'
+version          '0.1.0'
